5e967cb1b70911e9fce4547c8432676d0930fff9
FAITH <faithokereke26@gmail.com>
2024-07-03 17:22:15 +0100

    commit chnages

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let currentHour = new Date().getUTCHours();
     let currentMinute = new Date().getUTCMinutes();
+    console.log(currentMinute)
     const currentDay = new Date().getDay();
     const newHour = document.querySelector("p[data-testid='currentTimeUTC']");
-
-    let d = new Date();
     const days = [
         "Sunday",
         "Monday",
@@ -17,10 +16,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const newDay = document.querySelector("p[data-testid='currentDay']");
 
     //Format Hours and Minutes to always have two digits
-    currentMinute = currentHour < 10 ? "0" + currentHour : currentHour;
+    currentHour = currentHour < 10 ? "0" + currentHour : currentHour;
 
     //Set Time to PM or AM
-     currentMinute= currentHour > 12? currentMinute +"PM" :currentMinute +"AM"
+    currentMinute = currentHour > 12 ? currentMinute + "PM" : currentMinute + "AM"
     //set the current time and day
     newHour.innerHTML = "Time: " + currentHour + ":" + currentMinute;
     newDay.innerHTML = "Day: " + days[currentDay];
